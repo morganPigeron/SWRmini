2ddce5e80e3068af680a85becf280f01fc643b13
Morgan Pigeron <m.pigeron@hotmail.fr>
2021-07-08 23:36:27 +0200

    added mouse tooltip

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,41 @@
 import { AmbientLight, GreaterEqualStencilFunc, Vector3 } from "three";
 import { ThreeJsService } from "./core/threeJsService";
 
-export async function app() { // now here we can use await without any problem
-
+export async function app() {
+  // now here we can use await without any problem
 
   const threeJsService = new ThreeJsService();
 
-  const galaxy = await threeJsService.loadGltfModel("assets/galaxy/scene.gltf",{tx:-11,ty:11,tz:-11,rx:Math.PI/2,ry:0,rz:0}); // offset coordinate is optional
+  const galaxy = await threeJsService.loadGltfModel(
+    "assets/galaxy/scene.gltf",
+    { tx: -11, ty: 11, tz: -11, rx: Math.PI / 2, ry: 0, rz: 0 }
+  ); // offset coordinate is optional
   const planet = await threeJsService.loadGltfModel(
     "assets/planets/planete1.gltf"
   );
 
   threeJsService.addlight(new AmbientLight(0x404040));
 
-  
   galaxy.scale.set(0.1, 0.1, 0.1);
   // add function to main update loop
-  threeJsService.addUpdate(()=>{
+  threeJsService.addUpdate(() => {
     galaxy.parent!.rotation.y += 0.001; // rotate from parent coordinate (global axis)
   });
-
 
   planet.position.set(5, 0, 0);
   planet.scale.set(0.1, 0.1, 0.1);
   // add another function to main update loop
-  threeJsService.addUpdate(()=>{
+  threeJsService.addUpdate(() => {
     planet.parent!.rotation.y += 0.01; // rotate from parent coordinate (global axis) (année)
     planet.rotateY(0.01); // (jour)
   });
 
+  //fun with mouse
 
+  let circle = document.getElementById("test")!;
+  document.addEventListener("mousemove", (e) => {
+    circle.style.left = e.pageX + 20 + "px";
+    circle.style.top = e.pageY + -20 + "px";
+    circle.innerHTML = `x:${e.pageX},y:${e.pageY}`;
+  });
 }
